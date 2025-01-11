dd23743cf60c626e52fea519e90e027f69de3caf
LINOWHITE123 <166272048+LINOWHITE123@users.noreply.github.com>
2025-01-11 16:19:27 -0500

    Update Start-instance.py

--- Python/Start-instance.py
+++ Python/Start-instance.py
@@ -43,6 +43,6 @@ def start_ec2_instances(vpc_id, exclude_instance_id, region='us-east-1'):
 if __name__ == "__main__":
     VPC_ID = "vpc-0abc1234def56789"  # Replace with your VPC ID
     EXCLUDE_INSTANCE_ID = "vi-0193957f72e6b01ee"  # Replace with the instance ID to exclude
-    REGION = "us-east-1"  # Replace with your region if different
+    REGION = "us-east-2"  # Replace with your region if different
 
     start_ec2_instances(VPC_ID, EXCLUDE_INSTANCE_ID, REGION)
